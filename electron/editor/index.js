5f4ae00e4971789be332f87a9105b3916d96018f
Blinking <ouyang-zhaoxing@qq.com>
2019-09-26 16:11:34 +0800

    关闭electron默认菜单栏

--- electron/editor/index.js
+++ electron/editor/index.js
@@ -1,14 +1,13 @@
 const electron = require("electron");
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
 function createWindow() { // 创建窗口
 
     let mainWindow = new BrowserWindow({
 
         title: "Inversion",
-        // show: false, center: true,
+        show: false, center: true,
         autoHideMenuBar: true,
-        skipTaskbar: true,
         width: 1333, height: 786,
         webPreferences: { nodeIntegration: true }
 
@@ -18,7 +17,10 @@ function createWindow() { // 创建窗口
     mainWindow.loadFile("index.html");
 
     // 当页面准备完成后显示
-    // mainWindow.once("ready-to-show", function () { mainWindow.show(); });
+    mainWindow.once("ready-to-show", function () { mainWindow.show(); });
+
+    // 关闭菜单栏
+    Menu.setApplicationMenu(null);
 
     // 当窗口被关闭
     mainWindow.on("closed", function () { mainWindow = null; app.quit(); });
